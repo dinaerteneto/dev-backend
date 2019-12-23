0d02a258d52d06fc772677b82952bfbbc2f3a6f9
Dinaerte Neto <>
2019-12-23 20:18:13 -0300

    corrigindo alguns arquivos para funcionar tanto com docker quanto no heroku.

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const config = require('../../config/database');
+const config = require("../../config/database");
 const db = {};
 
 const sequelize = new Sequelize(
@@ -14,13 +14,14 @@ const sequelize = new Sequelize(
   config
 );
 
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+    const model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
 
